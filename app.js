108e00324845074f371c946a3d0adaf1b8c533ee
Maciej Kurek <maciejk77@gmail.com>
2016-07-16 00:49:50 +0100

    Last push of the night, time to go to bed

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 function Counter(clicksLimit) {
   this.clicksLimit = clicksLimit;
+  this.clicked = false; 
+  console.log(this.clicked); // to be commented out
 };
 
 var firstCounter = new Counter(10);
 var secondCounter = new Counter(15);
 var thirdCounter = new Counter(1);
 
-Counter.prototype.reset = function() {
-  return firstCounter;
-  return secondCounter;
-  return thirdCounter;
-};
-
-Counter.prototype.down = function() {
+Counter.prototype.click = function() {
   
+// i.e. $('#first-button').style.backgroundColor = "blue" to switch button color
+
   if(this.clicksLimit >= 1 ) {
     
     this.clicksLimit--;
@@ -22,25 +20,28 @@ Counter.prototype.down = function() {
   } else if (this.clicksLimit === 0) {
 
     this.clicksLimit = 0;
-    console.log('Limit now is ZERO : ' + this.clicksLimit);
-  }  
+    console.log('Limit now is ZERO: ' + this.clicksLimit);
+  }
 };
+
+function switchColor(button) {
+  button.style.backgroundColor = "blue";
+} 
 
 // Event Listeners for all buttons
 var firstButton = document.getElementById('first-button');     
-firstButton.addEventListener('click', function() { firstCounter.down() }, false); 
+firstButton.addEventListener('click', function() { firstCounter.click() }, false); 
 
 var secondButton = document.getElementById('second-button');     
-secondButton.addEventListener('click', function() { secondCounter.down() }, false); 
+secondButton.addEventListener('click', function() { secondCounter.click() }, false); 
 
 var thirdButton = document.getElementById('third-button');     
-thirdButton.addEventListener('click', function() { thirdCounter.down() }, false); 
+thirdButton.addEventListener('click', function() { thirdCounter.click() }, false); 
 
 var resetButton = document.getElementById('reset-button');     
-resetButton.addEventListener('click', function() { 
-  
+resetButton.addEventListener('click', function() {   
   firstCounter.clicksLimit = 10; 
   secondCounter.clicksLimit = 15;
   thirdCounter.clicksLimit = 1; 
-
+  // this.clicked = false; switch to false again 
 }, false);
