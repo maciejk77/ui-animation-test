1956cd8d0a6b3ca6d03bce06892cb2a36f305ef2
Maciej Kurek <maciejk77@gmail.com>
2016-07-16 11:35:22 +0100

    Color changing, disabling button, reset completed, counter next to button to be done

--- app.js
+++ app.js
@@ -1,47 +1,66 @@
 function Counter(clicksLimit) {
   this.clicksLimit = clicksLimit;
-  this.clicked = false; 
-  console.log(this.clicked); // to be commented out
+  //this.color = "yellow"; 
+  // console.log(this.clicked); // to be commented out
 };
 
 var firstCounter = new Counter(10);
 var secondCounter = new Counter(15);
 var thirdCounter = new Counter(1);
 
-Counter.prototype.click = function() {
+Counter.prototype.click = function(button) {
   
-// i.e. $('#first-button').style.backgroundColor = "blue" to switch button color
+  // Color check and change
+  if(this.clicksLimit > 1) {
+    button.style.backgroundColor = "blue";
+  } else if(this.clicksLimit <= 1) {
+    button.style.backgroundColor = "red";
+    button.disabled = true;    
+  }
 
+  // Click limit counter
   if(this.clicksLimit >= 1 ) {
-    
     this.clicksLimit--;
     console.log('Limit now is: ' + this.clicksLimit);
-
   } else if (this.clicksLimit === 0) {
-
     this.clicksLimit = 0;
     console.log('Limit now is ZERO: ' + this.clicksLimit);
   }
 };
 
-function switchColor(button) {
-  button.style.backgroundColor = "blue";
-} 
-
-// Event Listeners for all buttons
-var firstButton = document.getElementById('first-button');     
-firstButton.addEventListener('click', function() { firstCounter.click() }, false); 
-
-var secondButton = document.getElementById('second-button');     
-secondButton.addEventListener('click', function() { secondCounter.click() }, false); 
-
-var thirdButton = document.getElementById('third-button');     
-thirdButton.addEventListener('click', function() { thirdCounter.click() }, false); 
-
-var resetButton = document.getElementById('reset-button');     
-resetButton.addEventListener('click', function() {   
+Counter.prototype.reset = function() {
   firstCounter.clicksLimit = 10; 
   secondCounter.clicksLimit = 15;
   thirdCounter.clicksLimit = 1; 
-  // this.clicked = false; switch to false again 
+
+  firstButton.style.backgroundColor = "yellow"; 
+  secondButton.style.backgroundColor = "yellow"; 
+  thirdButton.style.backgroundColor = "yellow"; 
+};
+
+// Event Listeners for all buttons
+var firstButton = document.getElementById('first-button');     
+firstButton.addEventListener('click', function() { firstCounter.click(firstButton) }, false); 
+
+var secondButton = document.getElementById('second-button');     
+secondButton.addEventListener('click', function() { secondCounter.click(secondButton) }, false); 
+
+var thirdButton = document.getElementById('third-button');     
+thirdButton.addEventListener('click', function() { thirdCounter.click(thirdButton) }, false); 
+
+var resetButton = document.getElementById('reset-button');     
+resetButton.addEventListener('click', function() {   
+  
+  firstCounter.clicksLimit = 10; 
+  secondCounter.clicksLimit = 15;
+  thirdCounter.clicksLimit = 1; 
+
+  firstButton.style.backgroundColor = "yellow"; 
+  secondButton.style.backgroundColor = "yellow"; 
+  thirdButton.style.backgroundColor = "yellow";
+
+  firstButton.disabled = false; 
+  secondButton.disabled = false; 
+  thirdButton.disabled = false;  
+
 }, false);
