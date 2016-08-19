3976a9d0502a57fd8b4ce69a2bbf04a3e489d51d
Maciej Kurek <maciejk77@gmail.com>
2016-08-19 23:08:54 +0100

    Progress bar added, styling needed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function clickCallback() {
   
   if(totalFunds + this.amount < targetFunds) {
     displayRemaining[0].innerHTML = 'You need ' + '&pound;' + (targetFunds - totalFunds) + ' more to reach your target';
-  
+    
   } else {
     displayRemaining[0].innerHTML = totalFunds + this.amount + ' TARGET REACHED!';
     
@@ -16,13 +16,14 @@ function clickCallback() {
 
   totalFunds += this.amount;
   display[0].innerText = totalFunds;
-
+  progress.setAttribute("value", totalFunds);
 };
 
 // Get references to all DOM elements
 var buttons = document.querySelectorAll(".js-button");
 var display = document.querySelectorAll(".js-display");
 var displayRemaining = document.querySelectorAll(".js-display-remaining");
+var progress = document.getElementsByTagName("progress")[0];
 
 // Iterate through list of buttons and initialise them with the needed values
 buttons.forEach(function (button, index) {
