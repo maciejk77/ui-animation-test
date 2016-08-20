600e8a1df8a8e166bd4f56d692ce58b0464367f8
Maciej Kurek <maciejk77@gmail.com>
2016-08-20 18:34:30 +0100

    Basic engine completed

---
+++ README.md
@@ -0,0 +1,8 @@
+##Requirements:
+
+- To build an app as per wireframe provided
+- To position app vertically and horizontally centered.
+- On page load progress bar to animate from 0 to current value (47 as per wireframe)
+- To add buttons for “£1”, “£5”, “£10” donations respectively.
+- To udate UI once value changes
+- To turn this app into visually stunning component (bonus points)
--- app.js
+++ app.js
@@ -1,31 +1,40 @@
 var targetFunds = 100;
-var totalFunds = 47;
+var startFunds = 47;
+var totalFunds = startFunds;
 
 function clickCallback() {
   
-  if(totalFunds + this.amount < targetFunds) {
-    displayRemaining[0].innerHTML = 'You need ' + '&pound;' + (targetFunds - totalFunds) + ' more to reach your target';
+  // Toping up funds based on the amount passed from the button
+  totalFunds += this.amount;
+
+  // As long as accumulated funds are below 100 (target funds to be collected)
+  if(totalFunds < targetFunds) {
+
+    displayRemaining[0].innerHTML = 'You need ' + '&pound;' + (targetFunds - totalFunds) + ' more to reach your target'; 
+    var procentage = 'width: ' + totalFunds + '%';
+  
+  // Once accumulated funds reach or are just over 100 (target funds to be collected)
+  } else if(totalFunds >= targetFunds) {
     
-  } else {
-    displayRemaining[0].innerHTML = totalFunds + this.amount + ' TARGET REACHED!';
-    
-    buttons.forEach(function(button, index) {
-      button.disabled = true;
-    });
+    displayRemaining[0].innerHTML = 'Target reached! ' + '&pound;' + totalFunds + ' in total' ;
+    var procentage = 'width: ' + targetFunds + '%';
+    disableButtons();     
   }
 
-  totalFunds += this.amount;
+  // Update display message with the accumulated amount
   display[0].innerText = totalFunds;
-  progress.setAttribute("value", totalFunds);
+  // Attach style set with procentage variable
+  progress.setAttribute('style', procentage);
+
 };
 
 // Get references to all DOM elements
 var buttons = document.querySelectorAll(".js-button");
 var display = document.querySelectorAll(".js-display");
 var displayRemaining = document.querySelectorAll(".js-display-remaining");
-var progress = document.getElementsByTagName("progress")[0];
+var progress = document.querySelector(".meter > span");
 
-// Iterate through list of buttons and initialise them with the needed values
+// Iterate through list of buttons and initialise them with the amounts
 buttons.forEach(function (button, index) {
   
   // Get the limit for this button from a custom data attribute in the button markup
@@ -36,4 +45,36 @@ buttons.forEach(function (button, index) {
 
 });
 
+function animateBar() { 
+  var position = 0;
+  
+  setInterval(function() {
+    if(position <= startFunds) { 
+      procentage = 'width: ' + position + '%';
+      progress.setAttribute('style', procentage);
+      position++;
+    } 
+  }, 2000/60);
+}
 
+function disableButtons() {
+ // Disabling buttons so they cannot be clicked after target of £100 is reached
+  buttons.forEach(function(button, index) {
+    button.disabled = true;
+  });
+}
+
+
+
+/* 
+
+// Version to disable button if the value on the button pressed is over the remaining amount, 
+// may continue with lower amount until max 100 is reached, was not sure whether on last click going over 100 is allowed, hence this script
+
+function clickCallback() {
+  
+  totalFunds + this.amount <= targetFunds ? totalFunds += this.amount : this.disabled = true;
+  display[0].innerText = totalFunds;
+
+};
+*/
